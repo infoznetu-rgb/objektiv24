0b7af752edd340b790647841b3b6f9f7987bf26b
infoznetu-rgb <infoznetu@gmail.com>
2026-09-17 11:26:45 +0200

    Obnov PWA cache pre novu navigaciu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='objektiv24-pwa-v1';
+const CACHE='objektiv24-pwa-v2';
 const CORE=['/','/index.html','/styles.css','/app.js','/clanok.html','/clanok.js','/pwa.js','/analytics.js','/manifest.webmanifest','/assets/app-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
