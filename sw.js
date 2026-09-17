21dfcba14a66f56ca52e943457c822945b8499c4
infoznetu-rgb <infoznetu@gmail.com>
2026-09-17 11:38:30 +0200

    Aktualizovať PWA cache pre kontaktnú stránku

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='objektiv24-pwa-v2';
-const CORE=['/','/index.html','/styles.css','/app.js','/clanok.html','/clanok.js','/pwa.js','/analytics.js','/manifest.webmanifest','/assets/app-icon.svg'];
+const CACHE='objektiv24-pwa-v3';
+const CORE=['/','/index.html','/styles.css','/app.js','/clanok.html','/clanok.js','/pwa.js','/analytics.js','/ako-pracujeme.html','/kontakt.html','/kontakt-odoslane.html','/manifest.webmanifest','/assets/app-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
