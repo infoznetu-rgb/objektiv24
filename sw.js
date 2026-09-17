77d9782560d9d3b3fd686e609342f7b253a10771
infoznetu-rgb <infoznetu@gmail.com>
2026-09-17 12:37:07 +0200

    Obnoviť PWA cache pre obrazový systém

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='objektiv24-pwa-v6';
-const CORE=['/','/index.html','/styles.css','/app.js','/clanok.html','/clanok.js','/pwa.js','/analytics.js','/ako-pracujeme.html','/kontakt.html','/kontakt-odoslane.html','/manifest.webmanifest','/assets/app-icon.svg'];
+const CACHE='objektiv24-pwa-v7';
+const CORE=['/','/index.html','/styles.css','/app.js','/clanok.html','/clanok.js','/pwa.js','/analytics.js','/image-meta-public.js','/ako-pracujeme.html','/kontakt.html','/kontakt-odoslane.html','/manifest.webmanifest','/assets/app-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
